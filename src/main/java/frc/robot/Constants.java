5d25be58d03e49b044ec1002bde93ae73a476cdc
Brenden Campbell <campbellbrendene@gmail.com>
2022-04-02 14:01:44 -0400

    Update to new chassis

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,38 +3,14 @@ package frc.robot;
 public final class Constants {
     public static final class CANMapping {
         // DriveTrain
-        public static final int VICTORSPX_DRIVE_FL = 1;
-        public static final int VICTORSPX_DRIVE_BL = 2;
-        public static final int VICTORSPX_DRIVE_FR = 3;
-        public static final int VICTORSPX_DRIVE_BR = 4;
-        public static final int PIGEON_IMU = 20;
-
-        // IntakeArm
-        public static final int SPARKMAX_INTAKEARM_WINCH = 44;
-        
-        // Intake
-        public static final int SPARKMAX_INTAKE_RUN = 46;
-
-        // Climber
-        public static final int SPARKMAX_CLIMBER_ARM_L = 42;
-        public static final int SPARKMAX_CLIMBER_ARM_R = 45;
-        public static final int SPARKMAX_CLIMBER_WINCH_L = 43;
-        public static final int SPARKMAX_CLIMBER_WINCH_R = 41;
-    }
-
-    public static final class DIOMapping {
-        public static final int ENCODER_DRIVE_L[] = { 0, 1 };
-        public static final int ENCODER_DRIVE_R[] = { 2, 3 };
+        public static final int VICTORSPX_DRIVE_BL = 1;
+        public static final int VICTORSPX_DRIVE_BR = 2;
+        public static final int VICTORSPX_DRIVE_FL = 3;
+        public static final int VICTORSPX_DRIVE_FR = 4;
     }
 
     public static final class ControllerMapping {
         public static final int JOYSTICK = 0;
         public static final int XBOX = 1;
-    }
-
-    public static final class Tunable {
-        // Intake
-        public static final double INTAKE_RUN_IN    = -0.4;
-        public static final double INTAKE_RUN_OUT   = 1.00;
     }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,37 +3,18 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.Constants.ControllerMapping;
-import frc.robot.commands.ClimberManualCommand;
-import frc.robot.commands.DriveAutoCommand;
-import frc.robot.commands.DriveManualCommand;
-import frc.robot.commands.IntakeArmAutoCommand;
-import frc.robot.commands.IntakeInCommand;
-import frc.robot.commands.IntakeOutCommand;
-import frc.robot.commands.IntakeArmAutoCommand.IntakeArmPosition;
-import frc.robot.commands.ArmManualCommand;
-import frc.robot.subsystems.Climber;
-import frc.robot.subsystems.DriveTrain;
-import frc.robot.subsystems.Intake;
-import frc.robot.subsystems.IntakeArm;
-import frc.robot.utils.ExtendedXboxController;
-import frc.robot.utils.InputNormalizer;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.Constants.ControllerMapping;
+import frc.robot.commands.DriveManualCommand;
+import frc.robot.subsystems.DriveTrain;
+// import frc.robot.utils.ExtendedXboxController;
 
 public class RobotContainer {
   private final Joystick driveJoystick = new Joystick(ControllerMapping.JOYSTICK);
-  private final ExtendedXboxController controller = new ExtendedXboxController(ControllerMapping.XBOX);
+  // private final ExtendedXboxController controller = new ExtendedXboxController(ControllerMapping.XBOX);
 
 
   private final DriveTrain driveTrain = new DriveTrain();
-  private final Intake intake = new Intake();
-  private final IntakeArm intakeArm = new IntakeArm();
-  private final Climber climber = new Climber();
 
 
   private final DriveManualCommand driveManualCommand = new DriveManualCommand(
@@ -42,77 +23,14 @@ public class RobotContainer {
     driveJoystick::getTop, driveJoystick::getTrigger
   );
 
-  private final ClimberManualCommand climberManualCommand = new ClimberManualCommand(
-    climber,
-    new InputNormalizer(() -> -controller.getLeftY(), 0.1, -0.6, 0.6), // arm
-    new InputNormalizer(() -> -controller.getRightY(), 0.1, -1.0, 1.0) // winch
-  );
-
-  private final Command autonSimpleRight = new SequentialCommandGroup(
-    new IntakeOutCommand(intake).raceWith(new WaitCommand(0.5)),
-    new ParallelCommandGroup(
-      new SequentialCommandGroup(
-        new DriveAutoCommand(driveTrain, -4, 0),
-        new DriveAutoCommand(driveTrain, 0, -80)
-      ),
-      new IntakeArmAutoCommand(intakeArm, IntakeArmPosition.DOWN)
-    ),
-    new DriveAutoCommand(driveTrain, 10, 0)
-  );
-
-  private final Command autonSimpleLeft = new SequentialCommandGroup(
-    new IntakeOutCommand(intake).raceWith(new WaitCommand(0.5)),
-    new ParallelCommandGroup(
-      new IntakeArmAutoCommand(intakeArm, IntakeArmPosition.DOWN),
-      new DriveAutoCommand(driveTrain, -10, 0)
-    )
-  );
-
-  private final Command autonComplexRight = new SequentialCommandGroup(
-    // new InstantCommand(intakeArm::enable, intake),
-    new ParallelCommandGroup(
-      new IntakeArmAutoCommand(intakeArm, IntakeArmPosition.DOWN),
-      new DriveAutoCommand(driveTrain, 0, 40) // positive = clockwise
-    ),
-    new ParallelRaceGroup(
-      new IntakeInCommand(intake),
-      new DriveAutoCommand(driveTrain, 4.1, -5)
-    ),
-    new ParallelCommandGroup(
-      new IntakeArmAutoCommand(intakeArm, IntakeArmPosition.UP),
-      new DriveAutoCommand(driveTrain, 0, 140)
-    ),
-    new DriveAutoCommand(driveTrain, 6, 0),
-    new IntakeOutCommand(intake).raceWith(new WaitCommand(0.5)),
-    new ParallelCommandGroup(
-      new SequentialCommandGroup(
-        new DriveAutoCommand(driveTrain, -4, 0),
-        new DriveAutoCommand(driveTrain, 0, -80)
-      ),
-      new IntakeArmAutoCommand(intakeArm, IntakeArmPosition.DOWN)
-    ),
-    new DriveAutoCommand(driveTrain, 10, 0)
-  );
-
   private final SendableChooser<Command> autonChooser = new SendableChooser<>();
-
-  private final InputNormalizer intakeArmInput = new InputNormalizer(
-    () -> controller.getLeftTriggerAxis() - controller.getRightTriggerAxis(), 0.02, -0.5, 0.5);
-  private final ArmManualCommand intakeArmManualCommand = new ArmManualCommand(intakeArm, intakeArmInput);
   
   public RobotContainer() {
-    autonChooser.addOption("autonSimpleRight", autonSimpleRight);
-    autonChooser.addOption("autonSimpleLeft", autonSimpleLeft);
-    autonChooser.addOption("autonComplexRight", autonComplexRight);
     SmartDashboard.putData("auton", autonChooser);
 
     configureButtonBindings();
 
     driveTrain.setDefaultCommand(driveManualCommand);
-    intakeArm.setDefaultCommand(intakeArmManualCommand);
-    climber.setDefaultCommand(climberManualCommand);
-  
-    intakeArm.zeroEncoder(); // TODO: ?
   }
 
   public void teleopInit() {
@@ -120,20 +38,6 @@ public class RobotContainer {
   }
 
   private void configureButtonBindings() {
-    controller.b_LeftBumper()
-      .whileActiveOnce(new IntakeInCommand(intake));
-    controller.b_RightBumper()
-      .whileActiveOnce(new IntakeOutCommand(intake));
-    // controller.b_A()
-    //   .whenPressed(new InstantCommand(intakeArm::enable, intake));
-    // controller.b_B()
-    //   .whenPressed(new InstantCommand(intakeArm::disable, intake));
-    controller.b_X()
-      .whenPressed(new IntakeArmAutoCommand(intakeArm, IntakeArmPosition.DOWN));
-    controller.b_Y()
-      .whenPressed(new IntakeArmAutoCommand(intakeArm, IntakeArmPosition.UP));
-    controller.b_Start()
-      .whenPressed(new InstantCommand(intakeArm::zeroEncoder, intakeArm));
     // controller.b_Back()
     //   .whenPressed(autonComplexRight);
   }
